717753c43f1b6b298d51f0a1a66f199d0c33e69f
Saidur <sr.rifat@gmail.com>
2020-05-18 14:58:29 -0600

    command added 2

--- installation_docker_amd64.sh
+++ installation_docker_amd64.sh
@@ -8,24 +8,24 @@ then
     then
         echo "docker existing"
     else
-        sudo apt update
-        sudo apt install apt-transport-https ca-certificates curl software-properties-common
+        sudo apt update -y
+        sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
         sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
         sudo apt update
         apt-cache policy docker-ce
-        sudo apt install docker-ce
+        sudo apt install docker-ce -y
         sudo systemctl status docker
         sudo usermod -aG docker ${USER}
     fi
 else
-  sudo apt update
-  sudo apt install apt-transport-https ca-certificates curl software-properties-common
+  sudo apt update -y
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-  sudo apt update
+  sudo apt update -y
   apt-cache policy docker-ce
-  sudo apt install docker-ce
+  sudo apt install docker-ce -y
   sudo systemctl status docker
   sudo usermod -aG docker ${USER}
 fi
