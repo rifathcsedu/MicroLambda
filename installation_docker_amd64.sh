a454734743530f432ed01d1efcb7f956ef18d6ba
Saidur <sr.rifat@gmail.com>
2020-05-18 14:55:53 -0600

    command added

---
+++ installation_docker_amd64.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+which docker
+
+if [ $? -eq 0 ]
+then
+    docker --version | grep "Docker version"
+    if [ $? -eq 0 ]
+    then
+        echo "docker existing"
+    else
+        sudo apt update
+        sudo apt install apt-transport-https ca-certificates curl software-properties-common
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+        sudo apt update
+        apt-cache policy docker-ce
+        sudo apt install docker-ce
+        sudo systemctl status docker
+        sudo usermod -aG docker ${USER}
+    fi
+else
+  sudo apt update
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  sudo apt update
+  apt-cache policy docker-ce
+  sudo apt install docker-ce
+  sudo systemctl status docker
+  sudo usermod -aG docker ${USER}
+fi
+#etc.
