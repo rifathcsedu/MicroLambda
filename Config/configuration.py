6ed0ebf3d3761e83056ecd825219c623cafdff5e
rifathcsedu <sr.rifat@gmail.com>
2020-08-18 16:59:11 -0600

    Air Pollution Done

--- Config/configuration.py
+++ Config/configuration.py
@@ -29,8 +29,8 @@ Topic = dict(
 )
 
 MicroLambda=dict(
-    #short_lambda=['240']
-    short_lambda=['1500','420','360','300','240','180']
+    short_lambda=['300','240','180']
+    #short_lambda=['420','360']
 
 )
 
